ff527aeaa993b74849fb4638fc27846834c96278
Jarrell H Walker <jwalker-burnam@mail.csuchico.edu>
2018-08-19 19:23:19 -0700

    containers

--- src/containers/App.js
+++ src/containers/App.js
@@ -27,7 +27,7 @@ class App extends Component {
     }
 
     componentDidMount() {
-        
+
         fetch('https://jsonplaceholder.typicode.com/users').then(response => {
             return response.json();
         }).then(users => {
